d9ddd1ced26192128abbd87c6067f38d66396fdd
Joel Román Arancibia <joelromanarancibia@gmail.com>
2025-04-05 20:24:28 -0300

    inicial

---
+++ tp2_1_1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define N 20
+
+int main(){
+    srand(time(NULL));
+    // codigo a completar
+    double vt[N];
+    
+    for(int i = 0;i<N; i++){
+        vt[i]=1+rand()%100;
+        printf("\t%.2f", vt[i]);
+    }
+    
+    return 0;
+}
